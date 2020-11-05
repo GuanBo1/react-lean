23a607d0e008a98c938445702ffd2dc839f42418
GuanBo1 <1615522909@qq.com>
2020-11-06 00:53:01 +0800

    antdesign 配置

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -409,6 +409,7 @@ module.exports = function (webpackEnv) {
                       },
                     },
                   ],
+                  ['import', {"libraryName": "antd", "style": "css"}],
                   isEnvDevelopment &&
                     shouldUseReactRefresh &&
                     require.resolve('react-refresh/babel'),
--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import React from 'react';
-import {BrowserRouter as Router, HashRouter, Switch,Route} from 'react-router-dom';
-import Home from './views/Home';
-import About from './views/About';
-import './App.scss'
-
-export default class App extends React.Component{
+import React, { Fragment } from "react";
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from "./views/Home";
+import About from "./views/About";
+import "./App.scss";
+import { DatePicker } from "antd";
+export default class App extends React.Component {
   constructor(params) {
     super(params);
-    this.state = {
-       
-    }
+    this.state = {};
   }
-  render(){
+  render() {
     return (
-      <div className='test'>
-        <h2>ddddd</h2>
-        <ul>
-          <li>dddd</li>
-          <li>dddd</li>
-          <li>dddd</li>
-          <li>dddd</li>
-        </ul>
-        <Router>
-          <HashRouter>
-            <Switch>
-              <Route component={Home} exact path="/"></Route>
-              <Route component={About} path="/about"></Route>
-            </Switch>
-          </HashRouter>
-        </Router>
-      </div>
-     
+      <Fragment>
+        <div className="test">
+          <DatePicker />
+          <Router>
+            <HashRouter>
+              <Switch>
+                <Route component={Home} exact path="/">
+                  {" "}
+                </Route>{" "}
+                <Route component={About} path="/about">
+                  {" "}
+                </Route>{" "}
+              </Switch>{" "}
+            </HashRouter>{" "}
+          </Router>{" "}
+        </div>{" "}
+      </Fragment>
     );
   }
 }
-
